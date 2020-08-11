c32b56be7e8b6e227de6cf362dade011be8656a2
305267382 <53901698+305267382@users.noreply.github.com>
2020-08-11 15:47:32 +0800

    20200811

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,8 +8,21 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-# fw876/helloworld
+# Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Lienol/openwrt-package
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# Add a feed source
+
+
+
+sed -i '$a src-git node https://github.com/nxhack/openwrt-node-packages.git' feeds.conf.default
+#sed -i '$a src-git video https://github.com/openwrt/video.git' feeds.conf.default
+sed -i '$a src-git targets https://github.com/openwrt/targets.git' feeds.conf.default
+sed -i '$a src-git management https://github.com/openwrt-management/packages.git' feeds.conf.default
+#sed -i '$a src-git oldpackages http://git.openwrt.org/packages.git' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng.git' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/xiaoqingfengATGH/feeds-lienol.git' feeds.conf.default
+sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
+sed -i '$a src-git homeclash https://github.com/xiaoqingfengATGH/homeclash.git' feeds.conf.default
+sed -i '$a src-git strongswanInDocker https://github.com/xiaoqingfengATGH/luci-app-strongswanInDocker.git' feeds.conf.default
